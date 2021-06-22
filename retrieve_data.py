377dee6e5d6948785071452d6a681d42199020ec
Kevin Genskowsky Jeanneret <kgenskowsky@gmail.com>
2021-06-21 20:52:29 -0400

    Added unit testing
    Added unit testing

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,12 +1,12 @@
 import zipfile
 import shutil
 
+
 def get_zip_url(_zip_url, _dest_path):
-  import requests, zipfile, io
-  r = requests.get(_zip_url)
-  z = zipfile.ZipFile(io.BytesIO(r.content))
-  z.extractall(_dest_path)
-  z.close()
-  macosx = f"{_dest_path}__MACOSX"
-  shutil.rmtree(f"{_dest_path}__MACOSX")
- 
+    import requests, zipfile, io
+    r = requests.get(_zip_url)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(_dest_path)
+    z.close()
+    macosx = f"{_dest_path}__MACOSX"
+    shutil.rmtree(f"{_dest_path}__MACOSX")
