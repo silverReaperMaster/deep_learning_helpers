3c208945775ea03b59538bc9f5c6861d7aa5dfcf
Kevin Genskowsky Jeanneret <kgenskowsky@gmail.com>
2021-06-22 08:41:13 -0400

    .

--- models.py
+++ models.py
@@ -3,9 +3,7 @@ def display_model_details(_model, level=0):
         extra = ' ' * level * 2
         print(f"{extra} {layer_number} {layer.name.ljust(35)} \t{layer.trainable}")
         if hasattr(layer, 'layers'):
-            more_layers = True
             if level < 10:
                 display_model_details(layer, level + 1)
                 print("\n")
-        else:
-            more_layers = False
+
