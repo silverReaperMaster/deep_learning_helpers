b139c468cb9540ba7946defa823682d2c12e1931
Kevin Genskowsky Jeanneret <kgenskowsky@gmail.com>
2021-06-24 12:35:13 -0400

    clean

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,12 +1,13 @@
-import zipfile
 import shutil
+import requests
+import zipfile
+import io
 
 
-def get_zip_url(_zip_url, _dest_path):
-    import requests, zipfile, io
+def get_zip_url(_zip_url, _destiny_path):
     r = requests.get(_zip_url)
     z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall(_dest_path)
+    z.extractall(_destiny_path)
     z.close()
-    macosx = f"{_dest_path}__MACOSX"
-    shutil.rmtree(f"{_dest_path}__MACOSX")
+    macosx = f"{_destiny_path}__MACOSX"
+    shutil.rmtree(f"{_destiny_path}__MACOSX")
