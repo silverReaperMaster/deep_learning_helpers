2109ca929f9888f04428efc7fc30f29c1cc0cbc6
Kevin Genskowsky Jeanneret <kgenskowsky@gmail.com>
2021-06-25 20:12:11 -0400

    Update images.py

--- images.py
+++ images.py
@@ -6,7 +6,7 @@ def display_image_from_file(_target_file, _title=""):
     img = mpimg.imread(_target_file)
     plt.imshow(img)
     plt.title(_title)
-    plt.axis("off");
+    plt.axis("off")
     print(f"Image shape:{img.shape}")
     return img
 
