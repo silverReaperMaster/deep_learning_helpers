29be57e8d13fd7270b7f38c3d3aaa8be98ed1f3e
Kevin Genskowsky Jeanneret <kgenskowsky@gmail.com>
2021-06-27 16:59:51 -0400

    Update os_helper.py

--- os_helper.py
+++ os_helper.py
@@ -3,6 +3,46 @@ import sys
 import subprocess
 import pkg_resources
 
+from urllib.request import urlopen
+import subprocess
+import sys
+import re
+from io import StringIO
+from bs4 import BeautifulSoup
+import pandas as pd
+import warnings
+
+
+def clean_gpu_name(_gpu_result_str):
+    name_regex_extract = re.compile("^(.*?)(?=-)")
+    name = re.match(name_regex_extract, _gpu_result_str).group(0)
+    return name
+
+
+def check_hw():
+    gpu_params = ["name"]
+    command_out = subprocess.run(["nvidia-smi",
+                                  f"--query-gpu={','.join(gpu_params)}", "--format=csv"],
+                                 stdout=subprocess.PIPE,
+                                 text=True, input="")
+    command_str_io = StringIO(command_out.stdout)
+    command_out_results = pd.read_csv(command_str_io, sep=",")
+    # print(command_out_results.head())
+    gpu_name = clean_gpu_name(command_out_results['name'].iloc[0])
+
+    url = "https://developer.nvidia.com/cuda-gpus#compute"
+    cuda_page = urlopen(url)
+    bs = BeautifulSoup(cuda_page, "html.parser")
+    a_element = bs.body.find('a', text=f"{gpu_name}")
+    row = a_element.parent.parent
+    compute_value = float(row.findChildren('td')[1].text)
+
+    print("\n\t***GPU values***")
+    print(f"GPU Name: [{gpu_name}] compute value :[{compute_value}] \n")
+
+    if compute_value < 7:
+        warnings.warn("Compute capability under 7.0 and is not expected to show a significant speedup")
+
 
 def delete_file(_file_path):
     try:
@@ -14,11 +54,6 @@ def delete_file(_file_path):
 def clean_files_filter(files_array):
     r = list(filter(lambda x: x.find("DS") < 0, files_array))
     return r
-
-
-# def full_path_array(_dir_path, _dir_name, _file_names_array):
-#     # files = list(map(lambda: file_name: f"{_dir_path}/{_dir_name}/{file_name}",_file_names_array))
-#     return files
 
 
 def info_dir(_dir_path="./"):
