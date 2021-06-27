07acd66c532e3dba376336c51c9fe1ce0db0055b
Kevin Genskowsky Jeanneret <kgenskowsky@gmail.com>
2021-06-27 17:55:08 -0400

    Update images.py

--- images.py
+++ images.py
@@ -3,29 +3,53 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as tic
 import math
 import matplotlib.image as mpimg
+import numpy
 
-def display_image_from_file(_target_files, _title="", _y_title="", _fig_size=(10,10), _columns=1):
-    if isinstance(_target_files,str):
-      img = mpimg.imread(_target_files)
-      plt.imshow(img)
-      plt.title(_title)
-      plt.axis("off")
-      return img
+
+def set_title(ax, title, index=None):
+    if isinstance(title, numpy.ndarray):
+        print("Seting Title")
+        ax.set(title=f"{title[index]}")
+
+    if isinstance(title, str):
+        ax.set(title=f"{title}")
+
+
+def display_image(_target_image, _title="", _y_title="", _fig_size=(10, 10), _columns=1):
+    if isinstance(_target_image, numpy.ndarray) and len(_target_image.shape) == 4:
+        fig = plt.figure(figsize=_fig_size, constrained_layout=True)
+        gs = fig.add_gridspec(ncols=_columns, nrows=math.ceil(len(_target_image) / _columns), hspace=0, wspace=0)
+        print(len(_target_image))
+        for i in range(len(_target_image)):
+            ax = fig.add_subplot(gs[math.floor(i / _columns), i % _columns])
+            ax.imshow(_target_image[i])
+            set_title(ax, _title, i)
+            plt.setp(ax.get_xticklabels(), visible=False)
+            plt.setp(ax.get_yticklabels(), visible=False)
+        plt.show()
+
+
+def display_image_from_file(_target_files, _title="", _y_title="", _fig_size=(10, 10), _columns=1):
+    if isinstance(_target_files, str):
+        img = mpimg.imread(_target_files)
+        plt.imshow(img)
+        plt.title(_title)
+        plt.axis("off")
+        return img
 
     if isinstance(_target_files, numpy.ndarray):
-      print("Array")
-      fig = plt.figure(figsize=_fig_size, constrained_layout=True)
-      gs = fig.add_gridspec(ncols=_columns,nrows=math.ceil(len(_target_files)/_columns), hspace=0, wspace=0)
-      for i in range(len(_target_files)):
-        img = mpimg.imread(_target_files[i])
-        ax = fig.add_subplot(gs[math.floor(i/_columns),i%_columns])
-        ax.imshow(img)
-        ax.set(title=f"{_title[i]}", ylabel=f"{_y_title[i]}")
-        plt.setp(ax.get_xticklabels(), visible=False)
-        plt.setp(ax.get_yticklabels(), visible=False)
+        print("Array")
+        fig = plt.figure(figsize=_fig_size, constrained_layout=True)
+        gs = fig.add_gridspec(ncols=_columns, nrows=math.ceil(len(_target_files) / _columns), hspace=0, wspace=0)
+        for i in range(len(_target_files)):
+            img = mpimg.imread(_target_files[i])
+            ax = fig.add_subplot(gs[math.floor(i / _columns), i % _columns])
+            ax.imshow(img)
+            ax.set(title=f"{_title[i]}", ylabel=f"{_y_title[i]}")
+            plt.setp(ax.get_xticklabels(), visible=False)
+            plt.setp(ax.get_yticklabels(), visible=False)
 
-      plt.show()
-
+        plt.show()
 
 
 def show_images_comparator(img_array_1, img_array_2, img_scale=4):
