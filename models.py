8152b9c44a7ebc0890ac2eeccbd4dbf5df1151c8
Kevin Genskowsky Jeanneret <kgenskowsky@gmail.com>
2021-06-28 15:18:56 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,12 @@
 def display_model_details(_model, level=0):
+    """
+    Display information for model Layers.
+
+  """
     for layer_number, layer in enumerate(_model.layers):
         extra = ' ' * level * 2
-        print(f"{extra} {layer_number} {layer.name.ljust(35)} \t{layer.trainable}")
+        print(f"{extra} {layer_number} {layer.name.ljust(35)} \t{layer.trainable}\t{layer.dtype}")
         if hasattr(layer, 'layers'):
             if level < 10:
                 display_model_details(layer, level + 1)
                 print("\n")
-
